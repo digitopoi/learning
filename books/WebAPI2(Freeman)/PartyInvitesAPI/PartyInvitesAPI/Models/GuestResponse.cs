ca396b699a02ae9865080c8938194dd8e1dcecaa
digitopoi <hubo85@gmail.com>
2017-11-02 15:12:43 -0500

    Add guest response model

---
+++ books/WebAPI2(Freeman)/PartyInvitesAPI/PartyInvitesAPI/Models/GuestResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvitesAPI.Models
+{
+    public class GuestResponse
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public bool? WillAttend { get; set; }
+    }
+}
