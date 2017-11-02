6b301e5511cb94cc8bf558bb36d9bf96feac18cf
digitopoi <hubo85@gmail.com>
2017-11-02 15:23:56 -0500

    Add home controller

---
+++ books/WebAPI2(Freeman)/PartyInvitesAPI/PartyInvitesAPI/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+using PartyInvitesAPI.Models;
+using System.Linq;
+
+namespace PartyInvitesAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Rsvp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Rsvp(GuestResponse response)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.Add(response);
+                return View("Thanks", response);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [ChildActionOnly]
+        public ActionResult Attendees()
+        {
+            return View(Repository.Responses.Where(x => x.WillAttend == true));
+        }
+    }
+}
