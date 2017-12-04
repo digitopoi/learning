36b1b3991d1c681b4ef346d06d4850cebc60ef70
digitopoi <hubo85@gmail.com>
2017-12-04 08:40:53 -0600

    Add test project

---
+++ pluralsight/UnitTesting/BasicsOfUnitTestingCSharp/MyClasses/MyClasses.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyClasses.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyClasses.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c39deaef-a19e-46b9-863c-581ec753aab5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- pluralsight/UnitTesting/BasicsOfUnitTestingCSharp/MyClasses/MyClasses/FileProcess.cs
+++ pluralsight/UnitTesting/BasicsOfUnitTestingCSharp/MyClasses/MyClasses/FileProcess.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.IO;
 
 namespace MyClasses
 {
     public class FileProcess 
     {
+        public bool FileExists(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("fileName");
+            }
+
+            return File.Exists(fileName);
+        }
+
     }
 }
