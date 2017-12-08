52ff250e94d6848a5f0105e06bc4640024a0db6b
digitopoi <hubo85@gmail.com>
2017-12-08 15:16:05 -0600

    Configure startup

--- other/auth0/WebApi/Startup.cs
+++ other/auth0/WebApi/Startup.cs
@@ -8,6 +8,7 @@ using Owin;
 using System.Linq;
 using Microsoft.Owin.Security.OAuth;
 using AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode;
+using Auth0.Owin;
 
 [assembly: OwinStartup(typeof(WebApi.Startup))]
 
@@ -17,6 +18,22 @@ namespace WebApi
     {
         public void Configuration(IAppBuilder app)
         {
+            var domain = $"https://{ConfigurationManager.AppSettings["Auth0Domain"]}/";
+            var apiIdentifier = ConfigurationManager.AppSettings["Auth0ApiIdentifier"];
+
+            var keyResolver = new OpenIdConnectSigningKeyResolver(domain);
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidAudience = apiIdentifier,
+                        ValidIssuer = domain,
+                        IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) => keyResolver.GetSigningKey(identifier)
+                    }
+                });
+
             // Configure Web API
             WebApiConfig.Configure(app);
         }
