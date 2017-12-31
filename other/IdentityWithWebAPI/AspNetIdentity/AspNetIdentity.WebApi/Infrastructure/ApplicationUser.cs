8b5f8625b0ddda5a3105c53e53c8d45338f7d800
digitopoi <hubo85@gmail.com>
2017-12-31 16:36:23 -0600

    Add application user

---
+++ other/IdentityWithWebAPI/AspNetIdentity/AspNetIdentity.WebApi/Infrastructure/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspNetIdentity.WebApi.Infrastructure
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        public byte Level { get; set; }
+
+        [Required]
+        public DateTime JoinDate { get; set; }
+    }
+}
