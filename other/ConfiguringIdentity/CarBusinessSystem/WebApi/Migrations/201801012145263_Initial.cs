eefd64a37c3a87d08a1ae159b89bc743dce464a2
digitopoi <hubo85@gmail.com>
2018-01-01 15:47:04 -0600

    Add initial migration

---
+++ other/ConfiguringIdentity/CarBusinessSystem/WebApi/Migrations/201801012145263_Initial.cs
@@ -0,0 +1,18 @@
+namespace WebApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "IsDeleted", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "IsDeleted");
+        }
+    }
+}
