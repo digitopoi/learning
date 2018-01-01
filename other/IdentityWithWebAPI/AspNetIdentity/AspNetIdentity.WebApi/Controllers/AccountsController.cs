2bf64deebb27166eb2dd0c4660aa0c3b18d9e96a
digitopoi <hubo85@gmail.com>
2017-12-31 19:17:56 -0600

    Add Accounts controller

---
+++ other/IdentityWithWebAPI/AspNetIdentity/AspNetIdentity.WebApi/Controllers/AccountsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AspNetIdentity.WebApi.Controllers
+{
+    [RoutePrefix("api/accounts")]
+    public class AccountsController : BaseApiController
+    {
+
+        [Route("users")]
+        public IHttpActionResult GetUsers()
+        {
+            return Ok(this.AppUserManager.Users.ToList().Select(u => this.TheModelFactory.Create(u)));
+        }
+
+        [Route("user/{id:guid}", Name = "GetUserById")]
+        public async Task<IHttpActionResult> GetUser(string Id)
+        {
+            var user = await this.AppUserManager.FindByIdAsync(Id);
+
+            if (user != null)
+            {
+                return Ok(this.TheModelFactory.Create(user));
+            }
+
+            return NotFound();
+
+        }
+
+        [Route("user/{username}")]
+        public async Task<IHttpActionResult> GetUserByName(string username)
+        {
+            var user = await this.AppUserManager.FindByNameAsync(username);
+
+            if (user != null)
+            {
+                return Ok(this.TheModelFactory.Create(user));
+            }
+
+            return NotFound();
+
+        }
+    }
+}
