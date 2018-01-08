4a1904e113db31c1185c4fdf2fafad4ab52ca06d
digitopoi <hubo85@gmail.com>
2018-01-08 11:48:48 -0600

    add weather service

--- other/angularChartJS/charts/src/app/weather.service.ts
+++ other/angularChartJS/charts/src/app/weather.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class WeatherService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) {
+    return this._http.get('http://samples.openweathermap.org/data/2.5/history/city?q=Fishers,IN&appid=b6907d289e10d714a6e88b30761fae22')
+      .map(result => result);
+  }
 
 }
