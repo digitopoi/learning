92432204fe58ec218820e9ce644d4ec64ba409b6
digitopoi <hubo85@gmail.com>
2018-01-09 05:44:55 -0600

    Add points of interest

--- pluralsight/ASPNETCore/Dockx-WebAPI/CityInfo/CityInfo.API/Models/CityDto.cs
+++ pluralsight/ASPNETCore/Dockx-WebAPI/CityInfo/CityInfo.API/Models/CityDto.cs
@@ -10,6 +10,15 @@ namespace CityInfo.API.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int NumberOfPointsOfInterest { get; set; }
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+                = new List<PointOfInterestDto>();
     }
 }
