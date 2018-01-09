4785cfa47ee5fc6eb1e42ba843f968ebfb95e0ca
digitopoi <hubo85@gmail.com>
2018-01-09 05:22:09 -0600

    Add data store

--- pluralsight/ASPNETCore/Dockx-WebAPI/CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ pluralsight/ASPNETCore/Dockx-WebAPI/CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -12,11 +12,7 @@ namespace CityInfo.API.Controllers
         [HttpGet()]
         public JsonResult GetCities()
         {
-            return new JsonResult(new List<object>()
-            {
-                new { id = 1, Name = "New York City" },
-                new { id = 2, Name = "Antwerp" }
-            });
+            return new JsonResult(CitiesDataStore.Current.Cities);
         }
     }
 }
--- pluralsight/ASPNETCore/Dockx-WebAPI/CityInfo/CityInfo.API/Models/CityDto.cs
+++ pluralsight/ASPNETCore/Dockx-WebAPI/CityInfo/CityInfo.API/Models/CityDto.cs
@@ -10,5 +10,6 @@ namespace CityInfo.API.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int NumberOfPointsOfInterest { get; set; }
     }
 }
