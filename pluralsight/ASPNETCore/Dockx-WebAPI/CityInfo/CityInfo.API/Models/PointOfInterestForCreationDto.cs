98ece2eef705d420de79d4562cf49ca77ad69350
digitopoi <hubo85@gmail.com>
2018-01-09 06:17:46 -0600

    Add point of interest create dto

---
+++ pluralsight/ASPNETCore/Dockx-WebAPI/CityInfo/CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Models
+{
+    public class PointOfInterestForCreationDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
--- pluralsight/ASPNETCore/Dockx-WebAPI/CityInfo/CityInfo.API/Startup.cs
+++ pluralsight/ASPNETCore/Dockx-WebAPI/CityInfo/CityInfo.API/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
 {
@@ -15,7 +17,19 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddMvcOptions(o => o.OutputFormatters.Add(
+                    new XmlDataContractSerializerOutputFormatter()));
+                //.AddJsonOptions(o =>
+                //{
+                //    if (o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var castedResolver = o.SerializerSettings.ContractResolver
+                //            as DefaultContractResolver;
+
+                //        castedResolver.NamingStrategy = null;
+                //    }
+                //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
