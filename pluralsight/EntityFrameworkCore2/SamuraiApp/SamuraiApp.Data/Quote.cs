293381202fe11798496e3bd56c54d0e5770b99b0
digitopoi <hubo85@gmail.com>
2018-02-19 17:04:56 -0600

    Add battle class

--- pluralsight/EntityFrameworkCore2/SamuraiApp/SamuraiApp.Data/Quote.cs
+++ pluralsight/EntityFrameworkCore2/SamuraiApp/SamuraiApp.Data/Quote.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SamuraiApp.Data
+﻿namespace SamuraiApp.Data
 {
     public class Quote
     {
