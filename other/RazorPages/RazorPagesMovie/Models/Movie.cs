c79af36c83bdba2a1720f30e2216fec4a8a68f2a
digitopoi <hubo85@gmail.com>
2018-02-19 15:13:56 -0600

    Add data annotations

--- other/RazorPages/RazorPagesMovie/Models/Movie.cs
+++ other/RazorPages/RazorPagesMovie/Models/Movie.cs
@@ -7,16 +7,25 @@ namespace RazorPagesMovie.Models
     {
         public int ID { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
         
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
         public string Rating { get; set; }
     }
 }
