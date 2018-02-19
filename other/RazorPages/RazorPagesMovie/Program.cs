fd02d61e819f5461bbf343e1bc7a63dc7666b48a
digitopoi <hubo85@gmail.com>
2018-02-19 13:36:01 -0600

    seed database

--- other/RazorPages/RazorPagesMovie/Program.cs
+++ other/RazorPages/RazorPagesMovie/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie
 {
@@ -14,7 +17,28 @@ namespace RazorPagesMovie
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<MovieContext>();
+
+                    context.Database.Migrate();
+
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
