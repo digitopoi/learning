fb04d2dc4914930f5bf4cfbd6122b085035ac5ba
digitopoi <hubo85@gmail.com>
2018-02-19 11:42:26 -0600

    Register db context as a service

--- other/RazorPages/RazorPagesMovie/Startup.cs
+++ other/RazorPages/RazorPagesMovie/Startup.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie
 {
@@ -21,6 +23,8 @@ namespace RazorPagesMovie
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MovieContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("MovieContext")));
             services.AddMvc();
         }
 
