b1d48e2bfe629b09d5ce9a6a5d8d89a0b89652ff
digitopoi <hubo85@gmail.com>
2018-02-20 11:56:40 -0600

    Add core project

---
+++ pluralsight/EntityFrameworkCore2/SamuraiApp/Core/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        public SamuraiContext(DbContextOptions<SamuraiContext> options)
+            : base(options)
+        { }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(
+        //        "Server = (localdb)\\mssqllocaldb; Database = SamuraiAppData; Trusted_Connection = True; ");
+        //}
+    }
+}
