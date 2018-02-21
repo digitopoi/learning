9fed125eb28a987ef71eb55ab3ff7ca8394a1d16
digitopoi <hubo85@gmail.com>
2018-02-21 12:58:53 -0600

    Add form

--- books/ProCore/PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ books/ProCore/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -1,37 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using PartyInvites.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
         {
-            return View();
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
         }
 
-        public IActionResult About()
+        public ViewResult RsvpForm()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ books/ProCore/PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
