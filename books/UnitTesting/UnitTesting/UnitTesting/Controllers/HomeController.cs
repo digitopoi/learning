19a59688cc7480a9c55a17e0ff85fd5122403b59
digitopoi <hubo85@gmail.com>
2018-03-04 17:16:20 -0600

    Add methods to home controller

--- books/UnitTesting/UnitTesting/UnitTesting/Controllers/HomeController.cs
+++ books/UnitTesting/UnitTesting/UnitTesting/Controllers/HomeController.cs
@@ -6,8 +6,20 @@ namespace UnitTesting.Controllers
 {
     public class HomeController : Controller
     {
+        SimpleRepository Repository = SimpleRepository.SharedRepository;
+
         public IActionResult Index()
-             => View(SimpleRepository.SharedRepository.Products
+             => View(Repository.Products
                  .Where(p => p?.Price < 50));
+
+        [HttpGet]
+        public IActionResult AddProduct() => View(new Product());
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            Repository.AddProduct(p);
+            return RedirectToAction("Index");
+        }
     }
 }
