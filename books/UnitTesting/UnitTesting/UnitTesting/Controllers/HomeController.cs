753eaa3a4170cf9728c708215480d0afe52a9f9a
digitopoi <hubo85@gmail.com>
2018-03-04 16:43:01 -0600

    Update home controller

--- books/UnitTesting/UnitTesting/UnitTesting/Controllers/HomeController.cs
+++ books/UnitTesting/UnitTesting/UnitTesting/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using UnitTesting.Models;
+using System.Linq;
 
 namespace UnitTesting.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
-             => View(SimpleRepository.SharedRepository.Products);
+             => View(SimpleRepository.SharedRepository.Products
+                 .Where(p => p.Price < 50));
     }
 }
