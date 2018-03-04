af6b3569088d73d461cfb038bbcf9475bda63709
digitopoi <hubo85@gmail.com>
2018-03-04 16:28:14 -0600

    Add home controller

---
+++ books/UnitTesting/UnitTesting/UnitTesting/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UnitTesting.Models;
+
+namespace UnitTesting.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+             => View(SimpleRepository.SharedRepository.Products);
+    }
+}
