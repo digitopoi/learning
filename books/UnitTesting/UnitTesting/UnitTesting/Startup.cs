2645a55db9ca3bd4f62c098551cdab9f17c88131
digitopoi <hubo85@gmail.com>
2018-03-04 16:43:51 -0600

    Add dev exception page'

--- books/UnitTesting/UnitTesting/UnitTesting/Startup.cs
+++ books/UnitTesting/UnitTesting/UnitTesting/Startup.cs
@@ -21,6 +21,7 @@ namespace UnitTesting
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseDeveloperExceptionPage();
             app.UseMvcWithDefaultRoute();
         }
     }
