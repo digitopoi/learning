4b47c8ea576aa838c3bb02691ada11752b8c8ed7
digitopoi <hubo85@gmail.com>
2018-03-04 16:26:27 -0600

    Add simple repo

---
+++ books/UnitTesting/UnitTesting/UnitTesting/Models/SimpleRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace UnitTesting.Models
+{
+    public class SimpleRepository
+    {
+        private static SimpleRepository sharedRepository = new SimpleRepository();
+        private Dictionary<string, Product> products
+            = new Dictionary<string, Product>();
+
+        public static SimpleRepository SharedRepository => sharedRepository;
+
+        public SimpleRepository()
+        {
+            var initialItems = new[]
+            {
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M },
+                new Product { Name = "Soccer ball", Price = 19.50M },
+                new Product { Name = "Corner flag", Price = 34.95M },
+            };
+            foreach (var p in initialItems)
+            {
+                AddProduct(p);
+            }
+        }
+
+        public IEnumerable<Product> Products => products.Values;
+
+        public void AddProduct(Product p) => products.Add(p.Name, p);
+    }
+}
