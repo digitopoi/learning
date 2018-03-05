4c387fa14bef6574e65face42272863306c8b455
digitopoi <hubo85@gmail.com>
2018-03-04 19:56:05 -0600

    Add comparer class

---
+++ books/UnitTesting/UnitTesting/UnitTesting.Tests/Comparer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.Tests
+{
+    public class Comparer
+    {
+        public static Comparer<U> Get<U>(Func<U, U, bool> func)
+        {
+            return new Comparer<U>(func);
+        }
+    }
+
+    public class Comparer<T> : Comparer, IEqualityComparer<T>
+    {
+        private Func<T, T, bool> comparisonFunction;
+
+        public Comparer(Func<T, T, bool> func)
+        {
+            comparisonFunction = func;
+        }
+
+        public bool Equal(T x, T y)
+        {
+            return comparisonFunction(x, y);
+        }
+
+        public bool Equals(T x, T y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
