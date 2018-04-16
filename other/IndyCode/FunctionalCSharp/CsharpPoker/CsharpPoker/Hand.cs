1c3fb91d420e5a62db64e34a9dd9db870877916f
digitopoi <hubo85@gmail.com>
2018-04-16 12:51:04 -0500

    Refactor

--- other/IndyCode/FunctionalCSharp/CsharpPoker/CsharpPoker/Hand.cs
+++ other/IndyCode/FunctionalCSharp/CsharpPoker/CsharpPoker/Hand.cs
@@ -15,47 +15,5 @@ namespace CsharpPoker
 
         public void Draw(Card card) => cards.Add(card);
 
-        public Card HighCard() =>
-            cards.Aggregate((highCard, nextCard) => nextCard.Value > highCard.Value ? nextCard : highCard);
-
-        //  Optional
-        //  The return early pattern can be replaced with ternary operators
-        //  then shortened to an expression-bodied member.
-        public HandRank GetHandRank() =>
-            HasRoyalFlush() ? HandRank.RoyalFlush :
-            HasStraightFlush() ? HandRank.StraightFlush :
-            HasStraight() ? HandRank.Straight :
-            HasFlush() ? HandRank.Flush :
-            HasFullHouse() ? HandRank.FullHouse :
-            HasFourOfAKind() ? HandRank.FourOfAKind :
-            HasThreeOfAKind() ? HandRank.ThreeOfAKind :
-            HasPair() ? HandRank.Pair :
-            HandRank.HighCard;
-
-        private bool HasFlush() =>
-            cards.All(c => cards.First().Suit == c.Suit);
-
-        private bool HasRoyalFlush() =>
-            HasFlush() && cards.All(c => c.Value > CardValue.Nine);
-
-        //  The Any LINQ method validates that there are dictionary items with a specified pair count value.
-        private bool HasOfAKind(int num) => cards.ToKindAndQuantities().Any(c => c.Value == num);
-
-        private bool HasPair() => HasOfAKind(2);
-        private bool HasThreeOfAKind() => HasOfAKind(3);
-        private bool HasFourOfAKind() => HasOfAKind(4);
-
-        private bool HasFullHouse() => HasThreeOfAKind() && HasPair();
-
-        //  The Zip and Skip LINQ methods are replaced by a custom extension method, SelectConsecutive
-        //  Select consecutive works like LINQ Select, except it can evaluate two consecutive items in a collection
-        //  This is done using a yield keyword, the source code is in EvalExtensions.cs
-        private bool HasStraight() =>
-            cards
-                .OrderBy(card => card.Value)
-                .SelectConsecutive((n, next) => n.Value + 1 == next.Value)
-                .All(value => value);
-
-        private bool HasStraightFlush() => HasStraight() && HasFlush();
     }
 }
---
+++ other/IndyCode/FunctionalCSharp/CsharpPoker/CsharpPoker/Tests/FiveCardPokerScorerTests.cs
@@ -0,0 +1,134 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CsharpPoker.Tests
+{
+    public class FiveCardPokerScorerTests
+    {
+        [Fact]
+        public void CanGetHighCard()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Seven, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Clubs));
+            hand.Draw(new Card(CardValue.Five, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.King, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.Two, CardSuit.Hearts));
+            FiveCardPokerScorer.HighCard(hand.Cards).Value.Should().Be(CardValue.King);
+        }
+
+        [Fact]
+        public void CanScoreHighCard()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Seven, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Clubs));
+            hand.Draw(new Card(CardValue.Five, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.King, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.Two, CardSuit.Hearts));
+            FiveCardPokerScorer.GetHandRank(hand.Cards).Should().Be(HandRank.HighCard);
+        }
+
+        [Fact]
+        public void CanScoreFlush()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Two, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Three, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ace, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Five, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Six, CardSuit.Spades));
+            FiveCardPokerScorer.GetHandRank(hand.Cards).Should().Be(HandRank.Flush);
+        }
+        [Fact]
+        public void CanScoreRoyalFlush()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Queen, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.King, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ace, CardSuit.Spades));
+            FiveCardPokerScorer.GetHandRank(hand.Cards).Should().Be(HandRank.RoyalFlush);
+        }
+
+        [Fact]
+        public void CanScorePair()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Clubs));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Nine, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.Ace, CardSuit.Spades));
+            FiveCardPokerScorer.GetHandRank(hand.Cards).Should().Be(HandRank.Pair);
+        }
+
+        [Fact]
+        public void CanScoreThreeOfAKind()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Clubs));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Nine, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Spades));
+            FiveCardPokerScorer.GetHandRank(hand.Cards).Should().Be(HandRank.ThreeOfAKind);
+        }
+        [Fact]
+        public void CanScoreFourOfAKind()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Clubs));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Spades));
+            FiveCardPokerScorer.GetHandRank(hand.Cards).Should().Be(HandRank.FourOfAKind);
+
+        }
+        [Fact]
+        public void CanScoreFullHouse()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Clubs));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Spades));
+            FiveCardPokerScorer.GetHandRank(hand.Cards).Should().Be(HandRank.FullHouse);
+
+        }
+
+        [Fact]
+        public void CanScoreStraight()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Clubs));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Queen, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.King, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.Ace, CardSuit.Spades));
+
+            FiveCardPokerScorer.GetHandRank(hand.Cards).Should().Be(HandRank.Straight);
+        }
+
+        [Fact]
+        public void CanScoreStraightUnordered()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Ace, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Queen, CardSuit.Clubs));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.King, CardSuit.Hearts));
+
+            FiveCardPokerScorer.GetHandRank(hand.Cards).Should().Be(HandRank.Straight);
+        }
+    }
+}
