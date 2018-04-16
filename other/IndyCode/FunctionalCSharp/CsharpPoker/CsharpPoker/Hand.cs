3d2354de9e7d0698774bf025103078597a534fed
digitopoi <hubo85@gmail.com>
2018-04-16 10:10:35 -0500

    Implement remaining hands

--- other/IndyCode/FunctionalCSharp/CsharpPoker/CsharpPoker/Hand.cs
+++ other/IndyCode/FunctionalCSharp/CsharpPoker/CsharpPoker/Hand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,12 +24,44 @@ namespace CsharpPoker
         public HandRank GetHandRank() =>
             HasRoyalFlush() ? HandRank.RoyalFlush :
             HasFlush() ? HandRank.Flush :
+            HasFullHouse() ? HandRank.FullHouse :
+            HasFourOfAKind() ? HandRank.ThreeOfAKind :
+            HasThreeOfAKind() ? HandRank.ThreeOfAKind :
+            HasPair() ? HandRank.Pair :
             HandRank.HighCard;
 
         private bool HasFlush() =>
             cards.All(c => cards.First().Suit == c.Suit);
 
-        public bool HasRoyalFlush() =>
+        private bool HasRoyalFlush() =>
             HasFlush() && cards.All(c => c.Value > CardValue.Nine);
+
+        //  The Any LINQ method validates that there are dictionary items with a specified pair count value.
+        private bool HasOfAKind(int num) => GetKindAndQuantities(cards).Any(c => c.Value == num);
+
+        private bool HasPair() => HasOfAKind(2);
+        private bool HasThreeOfAKind() => HasOfAKind(3);
+        private bool HasFourOfAKind() => HasOfAKind(4);
+
+        private bool HasFullHouse() => HasThreeOfAKind() && HasPair();
+
+        /* Since a collection of pairs needs to be built, a mutable collection must be temporarily created.
+         * Because the scope is limited, risk for side-effect is minimal. In addition, the ConcurrentDictionary's
+         * AddOrUpdate() method is Thread Safe. While the ConcurrentDictionary mutates state via AddOrUpdate(), 
+         * it does use a functional principle called a higher order function.
+         * Higher order functions are simply a function that accepts or returns another function.
+         * The AddOrUpdate() method accepts a function which delegates how the item is updated.
+         */
+        private IEnumerable<KeyValuePair<CardValue, int>> GetKindAndQuantities(IEnumerable<Card> cards)
+        {
+            var dict = new ConcurrentDictionary<CardValue, int>();
+            foreach (var card in cards)
+            {
+                //  Add the value to the dictionary, or increase the count
+                dict.AddOrUpdate(card.Value, 1, (cardValue, quantity) => ++quantity);
+            }
+
+            return dict;
+        }
     }
 }
--- other/IndyCode/FunctionalCSharp/CsharpPoker/CsharpPoker/Tests/HandTests.cs
+++ other/IndyCode/FunctionalCSharp/CsharpPoker/CsharpPoker/Tests/HandTests.cs
@@ -78,5 +78,57 @@ namespace CsharpPoker.Tests
 
             hand.GetHandRank().Should().Be(HandRank.RoyalFlush);
         }
+
+        [Fact]
+        public void CanScorePair()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Clubs));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Nine, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.Ace, CardSuit.Spades));
+
+            hand.GetHandRank().Should().Be(HandRank.Pair);
+        }
+
+        [Fact]
+        public void CanScoreThreeOfAKind()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Clubs));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Nine, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Spades));
+
+            hand.GetHandRank().Should().Be(HandRank.ThreeOfAKind);
+        }
+
+        [Fact]
+        public void CanScoreFourOfAKind()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Clubs));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Spades));
+
+            hand.GetHandRank().Should().Be(HandRank.FourOfAKind);
+
+        }
+        [Fact]
+        public void CanScoreFullHouse()
+        {
+            var hand = new Hand();
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Clubs));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Spades));
+            hand.Draw(new Card(CardValue.Jack, CardSuit.Hearts));
+            hand.Draw(new Card(CardValue.Ten, CardSuit.Spades));
+
+            hand.GetHandRank().Should().Be(HandRank.FullHouse);
+        }
     }
 }
