570cefa6054a1cb480bde8c0a55d22586e364c64
digitopoi <hubo85@gmail.com>
2018-04-19 08:29:39 -0500

    Setup test env

--- other/TDDFizzBuzz/FizzBuzz/FizzBuzz.Library.Tests/FizzBuzzerTests.cs
+++ other/TDDFizzBuzz/FizzBuzz/FizzBuzz.Library.Tests/FizzBuzzerTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace FizzBuzz.Library.Tests
 {
+    [TestFixture]
     public class FizzBuzzerTests
     {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.Pass();
+        }
     }
 }
